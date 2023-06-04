af5c76befbbad6dfcd5809abb9c2390cbd4df5cb
Sebastian <s.bednarek.03@gmail.com>
2023-06-04 23:16:58 +0200

    final

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -12,37 +11,15 @@ function App() {
         <h1>Sebastian Bednarek</h1>
         <h2>nr albumu: 113793</h2>
       </div>
-      <div>
-        <button>Home</button>
-        <button>Galeria</button>
-        <button>Wyszukaj</button>
-        <button>Logowanie</button>
-      </div>
-      <div>
-export default App;
-      </div>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <p></p>
+      <img src="https://www.national-geographic.pl/media/cache/slider_big/uploads/media/default/0014/44/best-of-the-world-2022-national-geographic-oto-najciekawsze-miejsca-ktore-warto-odwiedzic-w-2022-roku_3.png" alt="Zdjęcie Łodzi" width={700} height={400}></img>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default App;
